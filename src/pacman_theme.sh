dcac39000b4923b40d3657d14e695cab4d6cd4f4
Ruud Helderman <r.helderman@hccnet.nl>
2025-02-05 23:05:59 +0100

    Sawtooth for bass in pacman_theme

--- src/pacman_theme.sh
+++ src/pacman_theme.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
 # Imitation of Pac-Man intro theme music.
-# Bassline needs some work.
 
 D1=0.067
 D2=0.134
+D3=0.201
 D4=0.268
+D6=0.402
+D12=0.804
+D14=0.938
 
-BASE1="synth $D2 sin C3 pad 0 $D4 : synth $D2 sin G3"
-BASE2="synth $D2 sin C#3 pad 0 $D4 : synth $D2 sin G#3"
-BASE3="synth $D2 sin G3 pad 0 $D2 : synth $D2 sin A3 pad 0 $D2 : synth $D2 sin B3 pad 0 $D2 : synth $D2 sin C4 pad 0 $D2"
+BF="vol 0.3 fade 0 $D14 $D12 treble -20 trim 0"
+SF="vol 0.7 pad 1@$D1 trim 0"
 
-SOLO1="synth $D1 sin C5 pad 0 $D1 : synth $D1 sin C6 pad 0 $D1 : synth $D1 sin G5 pad 0 $D1 : synth $D1 sin E5 pad 0 $D1 : synth $D1 sin C6 : synth $D2 sin G5 pad 0 $D1 : synth $D2 sin E5 pad 0 $D2"
-SOLO2="synth $D1 sin C#5 pad 0 $D1 : synth $D1 sin C#6 pad 0 $D1 : synth $D1 sin G#5 pad 0 $D1 : synth $D1 sin F5 pad 0 $D1 : synth $D1 sin C#6 : synth $D2 sin G#5 pad 0 $D1 : synth $D2 sin F5 pad 0 $D2"
-SOLO3="synth $D1 sin D5 : synth $D1 sin D#5 : synth $D1 sin E5 pad 0 $D1 : synth $D1 sin E5 : synth $D1 sin F5 : synth $D1 sin F#5 pad 0 $D1 : synth $D1 sin F5 : synth $D1 sin F#5 : synth $D1 sin G5 pad 0 $D1 : synth $D2 sin C6 pad 0 $D2"
+BASE2="synth saw C#2 $BF $D6 : synth saw G#2 $BF $D2"
+BASE3="synth saw G2 $BF $D4 : synth saw A2 $BF $D4 : synth saw B2 $BF $D4 : synth saw C3 $BF $D4"
+BASE2+=" : $BASE2"
+BASE1=${BASE2//#/}
 
-sox -m -v0.7 "|sox -n -p $BASE1 : $BASE1 : $BASE2 : $BASE2 : $BASE1 : $BASE1 : $BASE3" -v0.3 "|sox -n -p $SOLO1 : $SOLO2 : $SOLO1 : $SOLO3" "$1"
+SOLO1="synth sin C5 $SF $D2 : synth sin C6 $SF $D2 : synth sin G5 $SF $D2 : synth sin E5 $SF $D2 : synth sin C6 $SF $D1 : synth sin G5 $SF $D3 : synth sin E5 $SF $D4"
+SOLO2="synth sin C#5 $SF $D2 : synth sin C#6 $SF $D2 : synth sin G#5 $SF $D2 : synth sin F5 $SF $D2 : synth sin C#6 $SF $D1 : synth sin G#5 $SF $D3 : synth sin F5 $SF $D4"
+SOLO3="synth sin D5 $SF $D1 : synth sin D#5 $SF $D1 : synth sin E5 $SF $D2 : synth sin E5 $SF $D1 : synth sin F5 $SF $D1 : synth sin F#5 $SF $D2 : synth sin F5 $SF $D1 : synth sin F#5 $SF $D1 : synth sin G5 $SF $D2 : synth sin C6 $SF $D4"
+
+sox -m "|sox -n -p $BASE1 : $BASE2 : $BASE1 : $BASE3" "|sox -n -p $SOLO1 : $SOLO2 : $SOLO1 : $SOLO3" "$1"
