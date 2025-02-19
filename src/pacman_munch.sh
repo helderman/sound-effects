a869227531215ef0ff6b6b330b2ac370768e84da
Ruud Helderman <r.helderman@hccnet.nl>
2025-02-19 20:02:36 +0100

    Increase sample rate; still a bit muffled though...

--- src/pacman_munch.sh
+++ src/pacman_munch.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Pac-Man 'wakka' munch sound
-# Wave form made with phase modulation: sin(4*f*t + 3*sin(2*f*t + 0.5*sin(4*f*t)))
-# Frequency f increasing in 5 steps (17 ms each), silence 65 ms, f decreasing in 5 steps
+# Pac-Man 'waka' munch sound - not 100% accurate but comes pretty close
+# Reverse-engineered simply by studying the wave forms of a PCM recording of the original sound
+# Ruud Helderman, Jan-Feb 2025 - MIT License
+# Wave form made with phase modulation: sin(4*pi*f*t + 3*sin(2*pi*f*t + 0.5*sin(4*pi*f*t)))
+# Stepwise increase f (165/235/305/375/445 Hz, each 16.78 ms), silence 66 ms, stepwise decrease f
 
-perl -e 'binmode(STDOUT);my$p=0;print pack"f*",map{$p+=0.13+0.055*int($_/136);$_<680?sin(2*$p+3*sin($p+sin(2*$p)/2)):0}(0..939,reverse 0..939)' \
-| sox -t raw -r 8k -e float -b 32 -c 1 -v 0.5 - "$1"
+perl -e 'binmode(STDOUT);my$p=0;print pack"f*",map{$p+=0.047+int($_/740)/50;$_<3700?sin($p+3*sin($p/2+sin($p)/2)):0}(0..5155,reverse 0..5155)' \
+| sox -t raw -r 44100 -e float -b 32 -c 1 -v 0.5 - "$1"
